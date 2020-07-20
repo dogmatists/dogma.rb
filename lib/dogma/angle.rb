5c25833527aea069c58e21a3e9b2b40981b39bf7
Arto Bendiken <arto@bendiken.net>
2020-07-20 17:24:49 +0300

    Implemented Angle#as_{degrees,radians,turns}.

--- lib/dogma/angle.rb
+++ lib/dogma/angle.rb
@@ -16,8 +16,8 @@ class Dogma::Angle
   def initialize(degrees: nil, radians: nil, turns: nil)
     @radians = case
       when radians then radians.to_f
-      when degrees then degrees.to_f / 180 * Math::PI
-      when turns then turns * 2 * Math::PI
+      when degrees then degrees.to_f/180 * Math::PI
+      when turns then turns * 2*Math::PI
     end
   end
 
@@ -25,5 +25,23 @@ class Dogma::Angle
   # @return [Float]
   def to_f
     self.radians
+  end
+
+  ##
+  # @return [Float]
+  def as_degrees
+    self.radians/Math::PI * 180
+  end
+
+  ##
+  # @return [Float]
+  def as_radians
+    self.radians
+  end
+
+  ##
+  # @return [Float]
+  def as_turns
+    self.radians / (2*Math::PI)
   end
 end # Dogma::Angle
--- spec/angle_spec.rb
+++ spec/angle_spec.rb
@@ -5,25 +5,28 @@ require_relative '../lib/dogma'
 include Dogma
 
 RSpec.describe Dogma::Angle do
-  describe ".new(radians: ...)" do
-    it "constructs the angle from radians" do
-      expect(Angle.new(radians: 0).to_f).to eq(0)
-      expect(Angle.new(radians: Math::PI).to_f).to eq(Math::PI)
-      expect(Angle.new(radians: 2*Math::PI).to_f).to eq(2*Math::PI)
-    end
-  end
-
   describe ".new(degrees: ...)" do
     it "constructs the angle from degrees" do
       expect(Angle.new(degrees: 0).to_f).to eq(0)
+      expect(Angle.new(degrees: 90).to_f).to eq(0.5*Math::PI)
       expect(Angle.new(degrees: 180).to_f).to eq(Math::PI)
       expect(Angle.new(degrees: 360).to_f).to eq(2*Math::PI)
+    end
+  end
+
+  describe ".new(radians: ...)" do
+    it "constructs the angle from radians" do
+      expect(Angle.new(radians: 0).to_f).to eq(0)
+      expect(Angle.new(radians: 0.5*Math::PI).to_f).to eq(0.5*Math::PI)
+      expect(Angle.new(radians: Math::PI).to_f).to eq(Math::PI)
+      expect(Angle.new(radians: 2*Math::PI).to_f).to eq(2*Math::PI)
     end
   end
 
   describe ".new(turns: ...)" do
     it "constructs the angle from turns" do
       expect(Angle.new(turns: 0).to_f).to eq(0)
+      expect(Angle.new(turns: 0.25).to_f).to eq(0.5*Math::PI)
       expect(Angle.new(turns: 0.5).to_f).to eq(Math::PI)
       expect(Angle.new(turns: 1).to_f).to eq(2*Math::PI)
     end
@@ -31,7 +34,34 @@ RSpec.describe Dogma::Angle do
 
   describe "#to_f" do
     it "returns the angle in radians" do
-      expect(Angle.new(radians: 2).to_f).to eq(2)
+      expect(Angle.new(radians: 3.1415).to_f).to eq(3.1415)
+    end
+  end
+
+  describe "#as_degrees" do
+    it "returns the angle in degrees" do
+      expect(Angle.new(degrees: 0).as_degrees).to eq(0)
+      expect(Angle.new(degrees: 90).as_degrees).to eq(90)
+      expect(Angle.new(degrees: 180).as_degrees).to eq(180)
+      expect(Angle.new(degrees: 360).as_degrees).to eq(360)
+    end
+  end
+
+  describe "#as_radians" do
+    it "returns the angle in radians" do
+      expect(Angle.new(degrees: 0).as_radians).to eq(0)
+      expect(Angle.new(degrees: 90).as_radians).to eq(0.5*Math::PI)
+      expect(Angle.new(degrees: 180).as_radians).to eq(Math::PI)
+      expect(Angle.new(degrees: 360).as_radians).to eq(2*Math::PI)
+    end
+  end
+
+  describe "#as_turns" do
+    it "returns the angle in turns" do
+      expect(Angle.new(degrees: 0).as_turns).to eq(0)
+      expect(Angle.new(degrees: 90).as_turns).to eq(0.25)
+      expect(Angle.new(degrees: 180).as_turns).to eq(0.5)
+      expect(Angle.new(degrees: 360).as_turns).to eq(1)
     end
   end
 end
